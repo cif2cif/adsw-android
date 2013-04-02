0fca77cfc58ab11342e207fb473fcc256935f451
Carlos A. Iglesias <cif@gsi.dit.upm.es>
2013-04-02 12:18:52 +0200

    Añadido menú.

--- 3-Interfaz/Calculadora/gen/es/upm/dit/adsw/calculadora/R.java
+++ 3-Interfaz/Calculadora/gen/es/upm/dit/adsw/calculadora/R.java
@@ -24,18 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int botonMultiplicar=0x7f060004;
-        public static final int botonSumar=0x7f060003;
-        public static final int resultado=0x7f060002;
-        public static final int valor1=0x7f060000;
-        public static final int valor2=0x7f060001;
+        public static final int botonMultiplicar=0x7f070004;
+        public static final int botonSumar=0x7f070003;
+        public static final int item_acerca=0x7f070006;
+        public static final int item_ayuda=0x7f070005;
+        public static final int resultado=0x7f070002;
+        public static final int valor1=0x7f070000;
+        public static final int valor2=0x7f070001;
     }
     public static final class layout {
         public static final int layout_calculadora=0x7f030000;
     }
+    public static final class menu {
+        public static final int menu_calculadora=0x7f060000;
+    }
     public static final class string {
+        public static final int acerca=0x7f05000d;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int ayuda=0x7f05000c;
         public static final int ayuda_valor_1=0x7f050003;
         public static final int ayuda_valor_2=0x7f050004;
         public static final int boton_multiplicar=0x7f050007;
@@ -43,6 +50,8 @@ public final class R {
         public static final int error_operar=0x7f050009;
         public static final int etiqueta_calculadora=0x7f050002;
         public static final int etiqueta_resultado=0x7f050005;
+        public static final int item_acerca=0x7f05000b;
+        public static final int item_ayuda=0x7f05000a;
         public static final int sin_resultado=0x7f050008;
     }
 }
--- 3-Interfaz/Calculadora/src/es/upm/dit/adsw/calculadora/CalculadoraActivity.java
+++ 3-Interfaz/Calculadora/src/es/upm/dit/adsw/calculadora/CalculadoraActivity.java
@@ -2,6 +2,10 @@ package es.upm.dit.adsw.calculadora;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,7 +14,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class CalculadoraActivity extends Activity {
-
+	private static final String TAG = "CalculadoraActivity";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,8 +25,8 @@ public class CalculadoraActivity extends Activity {
 		final TextView resultado = (TextView) findViewById(R.id.resultado);
 		Button botonSumar = (Button) findViewById(R.id.botonSumar);
 		Button botonMultiplicar = (Button) findViewById(R.id.botonMultiplicar);
-		
-		botonSumar.setOnClickListener( new OnClickListener() {	
+
+		botonSumar.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				try {
@@ -29,14 +34,16 @@ public class CalculadoraActivity extends Activity {
 					int val2 = Integer.parseInt(value2.getText().toString());
 					int suma = val1 + val2;
 					resultado.setText("" + suma);
-				}catch(Exception e) {
-					Toast.makeText(getApplicationContext(), getString(R.string.error_operar), Toast.LENGTH_SHORT).show();
+				} catch (Exception e) {
+					Toast.makeText(getApplicationContext(),
+							getString(R.string.error_operar),
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
-		
+
 		botonMultiplicar.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				int val1 = Integer.parseInt(value1.getText().toString());
@@ -44,7 +51,33 @@ public class CalculadoraActivity extends Activity {
 				int producto = val1 * val2;
 				resultado.setText("" + producto);
 			}
-		});		
+		});
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_calculadora, menu);
+		return true;
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.item_acerca: {
+				Toast.makeText(getApplicationContext(), getString(R.string.acerca), Toast.LENGTH_SHORT).show();
+				break;
+			}
+			case R.id.item_ayuda: {
+				Toast.makeText(getApplicationContext(), getString(R.string.ayuda), Toast.LENGTH_SHORT).show();
+				break;
+			}
+			default: {
+				Log.e(TAG, "Menú no previsto " + item.getItemId());
+			}
+		}
+		return false;
+
+	}
 }
